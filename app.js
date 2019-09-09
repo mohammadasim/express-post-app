fbf6eda41435e85a64e279e5578c815ac14cb6f3
Mohammad Ayub <mohammad.ayub@imagination.com>
2019-09-09 14:30:12 +0100

    added ej6 syntax

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var express = require("express");
-var request = require("request");
-var fake = require("faker");
-var bodyParser = require("body-parser");
-var app = express();
+const express = require("express");
+const rp = require("request-promise");
+const fake = require("faker");
+const bodyParser = require("body-parser");
+
+
+const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set("view engine", "ejs");
 var friendList = [];
+var movieList = [];
+
+
 
 app.get("/", (req, res) => {
     generateData();
@@ -26,7 +31,6 @@ app.get("/addfriend", (req, res) => {
 });
 
 app.post("/addaFriend", (req, res) => {
-    console.log(req.body);
     var newFriend = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -38,7 +42,39 @@ app.post("/addaFriend", (req, res) => {
     res.redirect("/friends");
 });
 
+app.get("/listmovies", (req, res) => {
+  res.render("movieList", {movieList:movieList});
+});
 
+app.post("/searchmovie", (req, res) => {
+    var options = {
+        uri: 'http://www.omdbapi.com',
+        qs: {
+            apikey: 'thewdb',
+            s: req.body.title
+        }
+    } 
+    rp(options)
+    .then((result) =>{
+        var parsedData = JSON.parse(result);
+        console.log(parsedData.Search);
+        parsedData.Search.forEach(function(searchResult){
+            var movie = {
+                title: searchResult.Title,
+                year: searchResult.Year,
+                imdbID: searchResult.imdbID,
+                type: searchResult.Type,
+                poster: searchResult.Poster  
+            }
+            movieList.push(movie);
+        });
+        console.log(movieList.length);
+    })
+    .catch((err) =>{
+        console.log('Error: ', err);
+    })
+  res.redirect("/listmovies");
+});
 
 const port = process.env.PORT || 5000;
 
